9480e61bd6a4c271544224911440f89a68a13498
FirCables <leleregina14@gmail.com>
2025-02-04 09:41:37 +0100

    yay

---
+++ src/components/OraCard.jsx
@@ -0,0 +1,12 @@
+const Oracard = (props) => {
+    return   (<article className="ora">
+    <header>
+      <h3>{props.oraSzam}</h3>
+      <button className="icon-button">📝</button>
+      <button className="icon-button">🗑️</button>
+    </header>
+    <h4>{props.cim}</h4>
+    <p>{props.children}</p>
+  </article>)
+};
+export default Oracard;
